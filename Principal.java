d783273c82557b59f19c8b7a8fb2c08a1e025898
Oscar_Paiz <oscarpaiz79@gmail.com>
2019-02-16 20:56:34 -0600

    Agregando fecha actual

--- Principal.java
+++ Principal.java
@@ -1,7 +1,14 @@
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class Principal{
+
+	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-		System.out.println("Hola mundo 2");
-		System.out.println("Hola mundo 3");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date));
 	}
 }
