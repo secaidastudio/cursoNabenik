238b0b93591d333af5b72fcde856b4e427cd636d
Oscar_Paiz <oscarpaiz79@gmail.com>
2019-02-16 21:06:59 -0600

    Agregando par-impar

--- Principal.java
+++ Principal.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class Principal{
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-		System.out.println("Hola mundo 2");
-		System.out.println("Hola mundo 3");
+		Scanner read = new Scanner (System.in);
+		int dato;
+
+		System.out.print("Ingrese numero: ");
+		dato = read.nextInt();
+
+		if ((dato % 2)==0 ) {
+			System.out.println("Numero par");
+		} else {
+			System.out.println("NUmero impar");
+		}
+
+
+		
+
 	}
 }
